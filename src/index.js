1a4c9aaf8bd8ea556585d53b6e1ff26f3ef8eee5
Michael Iglesias <mike.iglesias11@gmail.com>
2017-12-06 11:35:05 -0500

    quick changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
+import AWS from 'aws-sdk';
 import _ from 'lodash';
 
-exports.main = (event, context, callback) => {
-  console.log('Moneypenny');
+const ec2 = new AWS.EC2({
+  apiVersion: '2016-11-15'
+});
 
-  cb();
+exports.main = (event, context, callback) => {
+  console.log('Moneypenny: ', Date.now());
+
+  getMoneypennyManagedInstances().then(res => {
+    console.log(res, 'result');
+
+    callback(null, res);
+  });
 };
+
+function getMoneypennyManagedInstances() {
+  const params = {
+    Filters: [
+      {
+        Name: 'tag:Moneypenny',
+        Values: [
+          'managed'
+        ]
+      },
+    ]
+  };
+
+  return new Promise((resolve, reject) => {
+    ec2.describeInstances(params, function(err, data) {
+      if (err) reject(err);
+      else     resolve(data);
+    });
+  });
+}
